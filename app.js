e1315705d3d93b9ddc031e707dcb2e4a66f15648
aprietof <adrianprieto00@gmail.com>
2017-04-09 02:40:09 -0400

    socket io events added to upload video and get response

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express')
 , Youtube = require("youtube-api")
 , io = require('socket.io')(server)
 , gapi = require('./lib/gapi')
-, fs = require("fs")
 , readJson = require("r-json")
 , Logger = require("bug-killer")
 , opn = require('opn')
@@ -31,4 +30,76 @@ server.listen(5000, ()=> {
 
 app.get('/', function(req, res,next) {  
   res.sendFile(__dirname + '/public/views/index.html');
+});
+
+
+/*****************************************************
+ *             SOCKET.IO EVENT LISTENERS            *
+ ****************************************************/
+
+let currentVideo = {};
+
+io.on('connection', function(socket){
+
+  socket.on('start', function(video){
+
+    currentVideo = video
+
+    opn(gapi.oauth.generateAuthUrl({
+        access_type: "offline"
+      , scope: ["https://www.googleapis.com/auth/youtube.upload"]
+    }));
+
+    app.get('/oauth2callback', function(req, res) {
+      let code = req.query.code;
+
+      Logger.log("Trying to get the token using the following code: " + code);
+      
+      gapi.oauth.getToken(code, (err, tokens) => {
+
+        if (err) {
+            console.error(err)
+            res.status(500).send(err)
+            return Logger.log(err);
+        }
+
+        Logger.log("Got the tokens.");
+
+        gapi.oauth.setCredentials(tokens);
+
+        res.send("The video is being uploaded. Check out the logs in the terminal.");
+
+        let req = Youtube.videos.insert({
+            resource: {
+                // Video title and description
+                snippet: {
+                    title: currentVideo.title
+                  , description: currentVideo.description
+                }
+               
+              , status: {
+                    privacyStatus: currentVideo.privacyStatus
+                }
+            }
+            // This is for the callback function
+          , part: "snippet,status"
+
+          , media: {
+                body: currentVideo.video
+            }
+        }, (err, data) => {
+            if (data) { 
+              socket.emit('done', currentVideo);
+              spinner.stop(true)
+              Logger.log('Done!')
+            }
+            if (err) { 
+              console.error(err)
+            }
+        });
+        spinner.start();
+      });
+    });
+
+  });
 });
--- public/js/index.js
+++ public/js/index.js
@@ -38,6 +38,7 @@ $(function() {
       selectedFile.privacyStatus = privacyStatus.find(":selected").text().toLowerCase();
     }
 
+
     /*****************************************************
      *                  LISTENERS                        *
      ****************************************************/
@@ -56,10 +57,24 @@ $(function() {
 
       addFileInfo();
       resetInputFields();
+      
+      // Emit event via socket.io
       console.log(selectedFile)
+      socket.emit('start', selectedFile);
       selectedFile.video = undefined;
-
+      return false;
     });
 
 
+    // Add List of recent uploads
+    socket.on('done', function(video){
+      var liContent = '<li class="collection-item avatar"><i class="material-icons circle red">play_arrow</i><span class="title">Title</span><p>'+ video.title+'</p></li>'
+      
+      if($('#recentUploads').has('p')){ 
+        $('#empty').remove(); // Remove "No videos Uploaded Yet"
+      }
+      uploads.append(liContent);
+      Materialize.toast('<span>Video uploaded successfully &nbsp;<i class="material-icons toast-icon" style="color:#46b705;">check_circle</i></span>', 4000)
+    });
 });
+
