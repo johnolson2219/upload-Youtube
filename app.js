f8c6c2a93ea0e338ab1f33b1347dfdd78a46d97a
rajanram13 <rajanram13@banit.me>
2018-07-21 14:31:25 -0400

    download frm url

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
-const express = require('express')
-, app = express()
-, server = require('http').createServer(app)
-, Youtube = require("youtube-api")
-, io = require('socket.io')(server)
-, gapi = require('./lib/gapi')
-, readJson = require("r-json")
-, Logger = require("bug-killer")
-, opn = require('opn')
-, Spinner = require('cli-spinner').Spinner
-, spinner = new Spinner('uploading.. %s')
-  spinner.setSpinnerString('|/-\\');
+// jshint ignore:start
+const express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  Youtube = require("youtube-api"),
+  io = require('socket.io')(server),
+  gapi = require('./lib/gapi'),
+  readJson = require("r-json"),
+  Logger = require("bug-killer"),
+  Spinner = require('cli-spinner').Spinner,
+  spinner = new Spinner('uploading.. %s')
+session = require("express-session"),
+  fs = require('fs'), {
+    prepareDownload
+  } = require("./utils"),
+  download = require('download');
+var bodyParser = require('body-parser')
+
+spinner.setSpinnerString('|/-\\')
 
 
 /*****************************************************
  *                      APP                          *
  ****************************************************/
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(function(req, res, next) {
+  Logger.log(req.originalUrl);
+  next()
+})
 
+app.use(session({
+  secret: 'krunal',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 app.use(express.static(__dirname + '/public'));
-server.listen(5000, ()=> {
+server.listen(5000, () => {
   console.log('Youtube uploader app initiated! - localhost:5000');
-});  
+});
+
+app.locals.videos = [];
+
 
 
 /*****************************************************
@@ -28,78 +51,65 @@ server.listen(5000, ()=> {
  ****************************************************/
 
 
-app.get('/', function(req, res,next) {  
-  res.sendFile(__dirname + '/public/views/index.html');
+app.get('/', function(req, res, next) {
+  console.log(req.session);
+  res.sendFile(__dirname + '/public/views/login.html');
 });
 
+app.get("/auth", function(req, res) {
+  const redirectUri = gapi.oauth.generateAuthUrl({
+    access_type: "offline",
+    scope: ["https://www.googleapis.com/auth/youtube.upload"]
+  });
+  res.redirect(redirectUri);
+})
+
+app.get('/oauth2cb', async function(req, res) {
+  let code = req.query.code;
+  gapi.oauth.getToken(code, function(err, tokens) {
+    if (err) {
+      console.error(err)
+      return res.end(err);
+    }
+    req.session.tokens = tokens;
+    res.redirect("/home");
+  });
+
+});
+
+
+app.post('/start', function(req, res, next) {
+  const videos = req.body.videos;
+  console.log(req.body);
+  if (req.session.tokens) {
+    prepareDownload({
+      socket: app.locals.socket,
+      videos, tokens: req.session.tokens
+    }).then(d=>console.log(d)).catch(console.error);
+    res.end("uploading started");
+  } else res.end("please authenticate first");
+});
+
+
+app.get('/home', async function(req, res) {
+  if (req.session.tokens)
+    res.sendFile(__dirname + '/public/views/index.html');
+  else res.redirect("/");
+});
 
 /*****************************************************
  *             SOCKET.IO EVENT LISTENERS            *
  ****************************************************/
 
-let currentVideo = {};
 
-io.on('connection', function(socket){
 
-  socket.on('start', function(video){
+io.on('connection', function(socket) {
+  app.locals.socket = socket;
+});
 
-    currentVideo = video
 
-    opn(gapi.oauth.generateAuthUrl({
-        access_type: "offline"
-      , scope: ["https://www.googleapis.com/auth/youtube.upload"]
-    }));
 
-    app.get('/oauth2callback', function(req, res) {
-      let code = req.query.code;
-
-      Logger.log("Trying to get the token using the following code: " + code);
-      
-      gapi.oauth.getToken(code, (err, tokens) => {
-
-        if (err) {
-            console.error(err)
-            res.status(500).send(err)
-            return Logger.log(err);
-        }
-
-        Logger.log("Got the tokens.");
-
-        gapi.oauth.setCredentials(tokens);
-
-        res.send("The video is being uploaded. Check out the logs in the terminal.");
-
-        let req = Youtube.videos.insert({
-            resource: {
-                // Video title and description
-                snippet: {
-                    title: currentVideo.title
-                  , description: currentVideo.description
-                }
-               
-              , status: {
-                    privacyStatus: currentVideo.privacyStatus
-                }
-            }
-            // This is for the callback function
-          , part: "snippet,status"
-
-          , media: {
-                body: currentVideo.video
-            }
-        }, (err, data) => {
-            if (data) { 
-              socket.emit('done', currentVideo);
-              spinner.stop(true)
-              Logger.log('Done!')
-            }
-            if (err) { 
-              console.error(err)
-            }
-        });
-        spinner.start();
-      });
-    });
-
-  });
+process.on('uncaughtException', function(err) {
+  console.error(err.stack);
+  process.exit();
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,80 +1,52 @@
 $(function() {
-    const socket = io();
-    var fileBox = $('#fileBox')
-    , fileBoxLabel = $('#fileBoxLabel')
-    , uploadButton = $('#uploadButton')
-    , selectedFile = {}
-    , defaultText
-    , title = $('#title')
-    , description = $('#description')
-    , uploads = $('#uploads')
-    , privacyStatus = $('#privacyStatus');
+  const socket = io();
+  var uploadButton = $('#uploadButton'),
+    addButton = $("#addButton"),
+    defaultText, title = $('#title'),
+    videos = [],
+    url = $('#url'),
+    uploads = $('#uploads'),
+    queueContainer = $("#videoQueue"),
+    privacyStatus = $('#privacyStatus');
 
+
+  function resetInputFields() {
+    url.val('');
+  }
 
   /*****************************************************
-   *                    FUNCTIONS                      *
+   *                  LISTENERS                        *
    ****************************************************/
 
-    
-    function fileChosen(evnt) {
-      selectedFile.video = evnt.target.files[0];
+  $('select').material_select();
 
-      // Text filler for title and description if left empty
-      defaultText = selectedFile.video.name.split('.').slice(0)[0];
+  addButton.click(function(e) {
+    const video = {
+      url: url.val(),
+      title: title.val(),
+      privacyStatus: privacyStatus.val()
     }
-
-    function resetInputFields(){
-      title.val('').removeClass('valid')
-      description.val('').removeClass('valid');
-      fileBoxLabel.val('').removeClass('valid');
-      fileBox.wrap('<form>').closest('form').get(0).reset();
-      fileBox.unwrap(); 
+    let liContent = '<li class="collection-item avatar"><span class="title">Title</span><p>' + video.title + '</p></li>'
+    if ($('#videoQueue').has('p')) {
+      $('#emptyQueue').remove(); // Remove "No videos Uploaded Yet"
     }
+    queueContainer.append(liContent);
+    videos.push(video);
+    resetInputFields();
+  })
+  uploadButton.click(function(e) {
+    $.post("/start", {videos}, (data)=>{
+      Materialize.toast(data);
+    })
+  });
 
+  // Add List of recent uploads
+  socket.on('done', function(video) {
 
-    function addFileInfo(){
-      selectedFile.title = title.val() || defaultText
-      selectedFile.description = description.val() || defaultText;
-      selectedFile.privacyStatus = privacyStatus.find(":selected").text().toLowerCase();
+    if ($('#recentUploads').has('p')) {
+      $('#empty').remove(); // Remove "No videos Uploaded Yet"
     }
-
-
-    /*****************************************************
-     *                  LISTENERS                        *
-     ****************************************************/
-
-    $('select').material_select();
-
-    fileBox.on('change', fileChosen);
-
-    uploadButton.click(function(e){
-
-      // Validate presence of file
-      if (selectedFile.video === undefined) { 
-        alert("Please select a file");
-        return
-      } 
-
-      addFileInfo();
-      resetInputFields();
-      
-      // Emit event via socket.io
-      console.log(selectedFile)
-      socket.emit('start', selectedFile);
-      selectedFile.video = undefined;
-      return false;
-    });
-
-
-    // Add List of recent uploads
-    socket.on('done', function(video){
-      var liContent = '<li class="collection-item avatar"><i class="material-icons circle red">play_arrow</i><span class="title">Title</span><p>'+ video.title+'</p></li>'
-      
-      if($('#recentUploads').has('p')){ 
-        $('#empty').remove(); // Remove "No videos Uploaded Yet"
-      }
-      uploads.append(liContent);
-      Materialize.toast('<span>Video uploaded successfully &nbsp;<i class="material-icons toast-icon" style="color:#46b705;">check_circle</i></span>', 4000)
-    });
+    uploads.append(liContent);
+    Materialize.toast('<span>Video uploaded successfully &nbsp;<i class="material-icons toast-icon" style="color:#46b705;">check_circle</i></span>', 4000)
+  });
 });
-
